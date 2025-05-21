ff4db65a142f22c836443292301a35d9d037d1b0
kyiwinhtin <kyiwinhtin.kyiwinhtin@gmail.com>
2025-05-21 15:14:57 +0700

    project done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,92 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import { ImageBackground, Image, Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const TabIcon = ({ focused , icon , title } : any) => {
+  if(focused) {
+    return (
+      <ImageBackground 
+      source={images.highlight} 
+      className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold">{ title }</Text>
+      </ImageBackground>
+    )
+  }
+    return (
+      <View className="size-full justify-center items-center mt-4 rounded-full">
+        <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+      </View>
+    )
+}
+const _Layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs screenOptions={{
+      tabBarShowLabel: false,
+      tabBarItemStyle: {
+       
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        
+      },
+      tabBarStyle: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: 70,
+        backgroundColor: '#0F0D23',
+        borderTopWidth: 0,
+        elevation: 0,
+      }
+    }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused}) => (
+            <TabIcon focused={ focused} icon={icons.home} title="Home"/>
+          )
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="search"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused}) => (
+            <TabIcon focused={ focused} icon={icons.search} title="Search"/>
+          )
+        }}
+      />
+       <Tabs.Screen
+        name="saved"
+        options={{
+   
+          headerShown: false,
+          tabBarIcon: ({ focused}) => (
+            <TabIcon focused={ focused} icon={icons.save} title="Saved"/>
+          )
+        }}
+      />
+       <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused}) => (
+            <TabIcon focused={ focused} icon={icons.person} title="Profile"/>
+          )
         }}
       />
     </Tabs>
-  );
+    
+  )
 }
+
+export default _Layout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,28 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+// import React, { Component } from 'react'
+// import { Text, View } from 'react-native'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+// import { Stack } from 'expo-router'
+// import "../global.css"
+
+// export default class _layout extends Component {
+//   render() {
+//     return <Stack>
+//             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+//             <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+//           </Stack>
+//   }
+// }
+
+import { Stack } from "expo-router";
+import "../global.css";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+    </Stack>
   );
 }
+  
+
